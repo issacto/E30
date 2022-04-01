734cc2079328f94d677b02f2fc6606d607c17c2d
issac-to <issac.to@ibm.com>
2022-04-01 10:26:56 +0100

    clean

--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,7 @@ import (
 	"E30/config"
 	"E30/handlers/song"
 	"E30/handlers/user"
-	"context"
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -35,38 +29,40 @@ func main() {
 		R: router,
 	})
 
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: router,
-	}
+	// srv := &http.Server{
+	// 	Addr:    ":8080",
+	// 	Handler: router,
+	// }
 
-	// Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Failed to initialize server: %v\n", err)
-		}
-	}()
+	router.Run("0.0.0.0:5000")
 
 	config.Connect()
 
-	log.Printf("Listening on port %v\n", srv.Addr)
+	// Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
+	// go func() {
+	// 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	// 		log.Fatalf("Failed to initialize server: %v\n", err)
+	// 	}
+	// }()
 
-	// Wait for kill signal of channel
-	quit := make(chan os.Signal)
+	// log.Printf("Listening on port %v\n", srv.Addr)
 
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// // Wait for kill signal of channel
+	// quit := make(chan os.Signal)
 
-	// This blocks until a signal is passed into the quit channel
-	<-quit
+	// signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	// // This blocks until a signal is passed into the quit channel
+	// <-quit
 
-	// Shutdown server
-	log.Println("Shutting down server...")
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v\n", err)
-	}
+	// // The context is used to inform the server it has 5 seconds to finish
+	// // the request it is currently handling
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
+
+	// // Shutdown server
+	// log.Println("Shutting down server...")
+	// if err := srv.Shutdown(ctx); err != nil {
+	// 	log.Fatalf("Server forced to shutdown: %v\n", err)
+	// }
 }
